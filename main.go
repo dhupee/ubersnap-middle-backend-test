2931f838be018da58917b5cc67ae343e6430f451
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-03-08 23:00:26 +0700

    create basic API

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Failed to load .env")
+		log.Println("Using default environment variables")
+	}
+
+	// Get the port from the environment variables
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	app := fiber.New()
+	v1 := app.Group("/v1")
+
+	v1.Get("/", RootHandler)
+	v1.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
+	})
+
+	log.Fatal(app.Listen(":" + PORT))
+}
+
+func RootHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+	// return c.SendFile("./assets/welcome.txt")
 }
