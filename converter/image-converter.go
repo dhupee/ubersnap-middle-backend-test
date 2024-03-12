d820a23a11c6065f7e7c0ba698d3410852d3afcb
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-03-12 15:47:38 +0700

    update the docs to match godoc standard

--- converter/image-converter.go
+++ converter/image-converter.go
@@ -5,8 +5,12 @@ import (
 )
 
 // ImageConvert converts the input image file to the specified output format using ffmpeg.
-// It takes the input file path and the output file path as arguments.
-// If the conversion is successful, it returns nil. Otherwise, it returns an error.
+// Parameters:
+// - input_path: The path to the input image file.
+// - output_path: The path to the output image file.
+//
+// Returns:
+// - nil if the conversion is successful, or an error if the conversion fails.
 func ImageConvert(input_path string, output_path string) error {
 	err := ffmpeg.Input(input_path).Output(output_path).OverWriteOutput().Run()
 	if err != nil {
