1b8205e3248e1db3a2a8c918673817fe44850dfe
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-03-12 14:00:04 +0700

    integrate upload and converter functions, can be improved

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	process "github.com/dhupee/ubersnap-middle-backend-test/converter"
+	converter "github.com/dhupee/ubersnap-middle-backend-test/converter"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -14,7 +16,8 @@ func main() {
 	// fmt.Println("Hello, World!")
 
 	// Make temporary directory if it doesn't exist
-	os.Mkdir("/tmp/ubersnap-backend", 0775)
+	tmpDir := "/tmp/ubersnap-backend"
+	os.Mkdir(tmpDir, 0775)
 
 	// Load the .env file
 	err := godotenv.Load()
@@ -29,7 +32,10 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		BodyLimit: 10 * 1024 * 1024, // 10 MB
+	})
+
 	v1 := app.Group("/v1")
 
 	v1.Get("/", RootHandler)
@@ -38,27 +44,64 @@ func main() {
 	})
 
 	// Route to receive file
-	v1.Post("/upload", func(c *fiber.Ctx) error {
+	v1.Post("/convert", func(c *fiber.Ctx) error {
+		fileTarget := c.Get("file-target")
+
 		// Parse the form file
 		image, err := c.FormFile("image")
 		if err != nil {
+			log.Println(err)
 			return err
 		}
 
-		tmpDir := "/tmp/ubersnap-backend/"
+		// extract image name without extension
+		imageName := strings.Split(image.Filename, ".")[0]
+		imageType := strings.Split(image.Filename, ".")[1]
+		log.Println(imageName)
+		log.Println(imageType)
+		log.Println(fileTarget)
 
-		// Save the file to the temporary directory
-		os.Create(tmpDir + image.Filename)
+		// if target directory doesn't exist, create it
+		if _, err := os.Stat(fmt.Sprintf(tmpDir + "/" + imageName)); os.IsNotExist(err) {
+			os.Mkdir(tmpDir+"/"+imageName, 0775)
+		}
+		// TODO:add else if if there's similar directory, add "directory-1" or "directory-n"
 
-		// Run a function to process the file
-		err = process.ImageConvert(image.Filename, "test.png")
+		// Save file to root directory:
+		err = c.SaveFile(image, fmt.Sprintf(tmpDir+"/"+imageName+"/input."+imageType))
 		if err != nil {
+			log.Println(err)
 			return err
 		}
 
-		return c.SendString("File uploaded and processed successfully")
+		inputPath := tmpDir + "/" + imageName + "/input." + imageType
+		outputPath := tmpDir + "/" + imageName + "/output." + fileTarget
+
+		err = converter.ImageConvert(inputPath, outputPath)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		return c.SendString("File uploaded and saved successfully")
+		// filename := image.Filename
+		//
+		// tmpDir := ("/tmp/ubersnap-backend/" + filename + "/")
+		//
+		// // Save the file to the temporary directory
+		// // os.Create(tmpDir + "original")
+		// c.SaveFile(image, tmpDir+"original.png")
+		//
+		// // // Run a function to process the file
+		// // err = process.ImageConvert(image.Filename, "test.png")
+		// // if err != nil {
+		// // 	return err
+		// // }
+		//
+		// return c.SendString("File uploaded and processed successfully")
 	})
 
+	// Start Fiber server
 	log.Fatal(app.Listen(":" + PORT))
 }
 
