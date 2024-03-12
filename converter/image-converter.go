d07b7313b10e138e5806c0e5439909a889e5a242
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-03-12 13:58:51 +0700

    add basic documentation on the functions

--- converter/image-converter.go
+++ converter/image-converter.go
@@ -4,6 +4,9 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
+// ImageConvert converts the input image file to the specified output format using ffmpeg.
+// It takes the input file path and the output file path as arguments.
+// If the conversion is successful, it returns nil. Otherwise, it returns an error.
 func ImageConvert(input_path string, output_path string) error {
 	err := ffmpeg.Input(input_path).Output(output_path).OverWriteOutput().Run()
 	if err != nil {
