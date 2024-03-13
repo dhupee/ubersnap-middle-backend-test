1d9a4b8bc8ce12901f10ad3afb005093d6067f11
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-03-14 00:04:10 +0700

    resize functions done
    in the handlers there are many functions that is duplicate, should be able to be put in a function i guess, for later

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	converter "github.com/dhupee/ubersnap-middle-backend-test/converter"
@@ -41,6 +42,7 @@ func main() {
 		return c.SendString("pong")
 	})
 	v1.Post("/convert", ConvertHandler)
+	v1.Post("/resize", ResizeHandler)
 
 	// // NOTE: This one is working but can be improved for the serving purposes
 	// v1.Get("/download/", func(c *fiber.Ctx) error {
@@ -69,6 +71,11 @@ func ConvertHandler(c *fiber.Ctx) error {
 
 	// Headers
 	fileTarget := c.Get("file-target")
+
+	// filetarget is required
+	if fileTarget == "" {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "File target is required", "data": nil})
+	}
 
 	// Make temporary directory if it doesn't exist
 	tmpDir := "/tmp/ubersnap-backend"
@@ -124,13 +131,94 @@ func ConvertHandler(c *fiber.Ctx) error {
 		return err
 	}
 
-	// curlDownloadLink := fmt.Sprintf(`curl --request GET --url http://127.0.0.1:8080/v1/download/ --header 'file-target: %s' --header 'image-name: %s'`, fileTarget, imageName)
-	// return c.SendString("File successfully converted, please download by curl: " + curlDownloadLink)
-	// return c.SendString("File uploaded and saved successfully")
-
 	// Send the output file
 	return c.SendFile(tmpDir + "/" + imageName + "/output." + fileTarget)
 
 	// NOTE: this is not ideal for production code,
 	// the correct way is to send the output file to storage bucket like S3 or GCS
+}
+
+func ResizeHandler(c *fiber.Ctx) error {
+	// Headers
+
+	width := c.Get("width")
+	height := c.Get("height")
+	fileTarget := c.Get("file-target")
+
+	// width and height is required
+	if width == "" || height == "" {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Width and height are required", "data": nil})
+	}
+
+	// Make temporary directory if it doesn't exist
+	tmpDir := "/tmp/ubersnap-backend"
+	if _, err := os.Stat(tmpDir); os.IsNotExist(err) {
+		os.Mkdir(tmpDir, 0775)
+	}
+
+	// This will be passed in the body of the request
+	image, err := c.FormFile("image")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Accepted file types
+	imageTypeList := []string{"jpg", "jpeg", "png", "webp"}
+
+	// extract image name without extension
+	imageName := strings.Split(image.Filename, ".")[0]
+	imageType := strings.Split(image.Filename, ".")[1]
+
+	// fileTarget is optional
+	if fileTarget == "" {
+		fileTarget = imageType
+	}
+
+	log.Println(imageName) // comment this once you dont need these
+	log.Println(imageType)
+	log.Println(fileTarget)
+
+	// Throw an error if the imageType and fileTarget is not in the list
+	// NOTE: In the future we can add API to scan for malware or something
+	if !utils.IsInSlice(imageType, imageTypeList) {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Invalid file type", "data": nil})
+	}
+	if !utils.IsInSlice(fileTarget, imageTypeList) {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Invalid file target", "data": nil})
+	}
+
+	// if target directory doesn't exist, create it
+	// TODO: Have better naming convention
+	if _, err := os.Stat(fmt.Sprintf(tmpDir + "/" + imageName)); os.IsNotExist(err) {
+		os.Mkdir(tmpDir+"/"+imageName, 0775)
+	}
+
+	// Save the file to the temporary directory
+	err = c.SaveFile(image, fmt.Sprintf(tmpDir+"/"+imageName+"/input."+imageType))
+	if err != nil {
+		return err
+	}
+
+	inputPath := tmpDir + "/" + imageName + "/input." + imageType
+	outputPath := tmpDir + "/" + imageName + "/output." + fileTarget
+
+	// convert width and height to int
+	widthInt, err := strconv.Atoi(width)
+	if err != nil {
+		return err
+	}
+	heightInt, err := strconv.Atoi(height)
+	if err != nil {
+		return err
+	}
+
+	err = converter.ImageResize(inputPath, outputPath, widthInt, heightInt)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Send the output file
+	return c.SendFile(tmpDir + "/" + imageName + "/output." + fileTarget)
 }
