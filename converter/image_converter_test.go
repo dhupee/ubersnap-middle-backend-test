86fefa8946307f25233fcb0d4df0306f9541a82e
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-03-14 21:53:22 +0700

    add test for image resize and change method on file format checking

--- converter/image_converter_test.go
+++ converter/image_converter_test.go
@@ -1,6 +1,9 @@
 package converter_test
 
 import (
+	"image"
+	_ "image/jpeg" // Replace with the appropriate image format package
+	_ "image/png"
 	"os"
 	"testing"
 
@@ -22,10 +25,52 @@ func TestImageConverter(t *testing.T) {
 		t.Error(err)
 	}
 
-	// check if the file exists
-	if _, err := os.Stat(output_path); os.IsNotExist(err) {
+	// check if the image is a jpeg
+	file, err := os.Open(output_path)
+	if err != nil {
 		t.Error(err)
+	}
+	defer file.Close()
+
+	_, format, err := image.DecodeConfig(file)
+	if err != nil {
+		t.Error(err)
+	}
+	if format != "jpeg" {
+		t.Error("Expected jpeg format, got", format)
 	}
 }
 
-// func TestImageResize
+func TestImageResize(t *testing.T) {
+	input_path := "../img/sample_1920x1280.png"
+	output_path := "/tmp/ubersnap-test/resized.png"
+	width_target := 200
+	height_target := 200
+
+	// if the output dir does not exist, create it
+	if _, err := os.Stat("/tmp/ubersnap-test"); os.IsNotExist(err) {
+		os.Mkdir("/tmp/ubersnap-test/", 0755)
+	}
+
+	// resize the image to 200x200
+	err := converter.ImageResize(input_path, output_path, width_target, height_target)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// check the width and height of the resized image
+	file, err := os.Open(output_path)
+	if err != nil {
+		t.Error(err)
+	}
+	defer file.Close()
+
+	config, _, err := image.DecodeConfig(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if config.Width != width_target || config.Height != height_target {
+		t.Error("Expected width and height to be", width_target, "and", height_target, "but got", config.Width, "and", config.Height)
+	}
+}
